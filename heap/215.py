2e4407fc44f6400f54e56fa01440855cdc90871b
Ondrej Vesely <veselyo182003@gmail.com>
2025-11-22 21:27:54 -0500

    heap med

---
+++ heap/215.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+            elif num > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+
+        return heap[0]
