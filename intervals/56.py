885ebbd47d0388a635b079ba50e3ec2e43c65f32
Ondrej Vesely <veselyo182003@gmail.com>
2025-11-28 13:02:23 -0500

    intervals med

---
+++ intervals/56.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x: x[0])
+        result = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            if (intervals[i][0] <= result[-1][1]):
+                result[-1][1] = max(intervals[i][1], result[-1][1])
+            else:
+                result.append(intervals[i])
+
+        return result
